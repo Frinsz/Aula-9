a37d2cbbeeab4663d751af0de9b5697addbc07bd
Frinsz <lucasfrins10@gmail.com>
2026-06-25 15:25:35 -0300

    Update aula9ex1.c

--- aula9ex1.c
+++ aula9ex1.c
@@ -3,10 +3,10 @@
 int main(){
     float dia[7], s=0, porc;
 
-    printf("choveu hoje? Digite 1 - Sim, 0 - Não\n");
+    printf("Reponda: 1 para Sim, 0 para Não\n");
 
     for (int i = 0; i<7; i++){
-        printf("Dia %d:", i+1);
+        printf("Choveu no Dia %d? ", i+1);
         scanf("%f", &dia[i]);
         if (dia[i] == 1){
             s++;
@@ -15,7 +15,7 @@ int main(){
     
     porc = s/7*100;
     
-    printf("Resultado: (%.0f%) de dias chuvosos.", porc);
+    printf("Resultado: (%.1f%) de dias chuvosos.", porc);
     
     
     return 0;
